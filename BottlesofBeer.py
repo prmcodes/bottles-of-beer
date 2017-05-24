def176a68942ee6c0a2c480ee0f1b544340ab477
PRM <prmwrites@gmail.com>
2017-05-24 18:44:43 -0400

    Loop now prints correct song

--- BottlesofBeer.py
+++ BottlesofBeer.py
@@ -2,6 +2,8 @@
 bottles_of_beer = 99
 
 # while loop 'sings' song from 99 bottles to 0 bottles. Edge case of "1 bottles"  needs to be dealt with.
-while bottles_of_beer > 0:
+while bottles_of_beer > 1:
   print("{0} bottles of beer on the wall, {0} bottles of beer! Take one down! pass it around! {1} bottles of beer on the wall!".format(bottles_of_beer, (bottles_of_beer-1)))
   bottles_of_beer-=1
+  if bottles_of_beer == 1:
+    print("{0} bottle of beer on the wall, {0} bottle of beer! Take one down! pass it around! No more bottles of beer on the wall!".format(bottles_of_beer))

bc3885d65309fbfe361176e1f6dea92c9415dd94
PRM <prmwrites@gmail.com>
2017-05-24 18:37:28 -0400

    Loop prints slightly incorrect song. Must fix line at 1 bottle.

--- BottlesofBeer.py
+++ BottlesofBeer.py
@@ -1,7 +1,7 @@
 # Start with 99 bottles of beer
 bottles_of_beer = 99
 
-#Basic while loop that counts down from 99 to 1
+# while loop 'sings' song from 99 bottles to 0 bottles. Edge case of "1 bottles"  needs to be dealt with.
 while bottles_of_beer > 0:
-  print("{0}".format(bottles_of_beer))
+  print("{0} bottles of beer on the wall, {0} bottles of beer! Take one down! pass it around! {1} bottles of beer on the wall!".format(bottles_of_beer, (bottles_of_beer-1)))
   bottles_of_beer-=1

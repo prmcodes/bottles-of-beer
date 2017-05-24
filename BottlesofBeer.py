9754027b1faeb0c6f4864fd0429adb0879e850af
PRM <prmwrites@gmail.com>
2017-05-24 18:32:25 -0400

    Added basic while loop

--- BottlesofBeer.py
+++ BottlesofBeer.py
@@ -1,2 +1,7 @@
 # Start with 99 bottles of beer
 bottles_of_beer = 99
+
+#Basic while loop that counts down from 99 to 1
+while bottles_of_beer > 0:
+  print("{0}".format(bottles_of_beer))
+  bottles_of_beer-=1
